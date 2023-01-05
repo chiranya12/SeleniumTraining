8c9c9e6876292318ea999824ea82562d44dd801e
chiranya12 <120159987+chiranya12@users.noreply.github.com>
2023-01-05 16:10:35 -0600

    added firefox browser support

--- Hello-World/src/test/java/selenium/Helloworld.java
+++ Hello-World/src/test/java/selenium/Helloworld.java
@@ -3,6 +3,7 @@ package selenium;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Helloworld {
 	public static void main(String[] args) {
@@ -14,5 +15,11 @@ public class Helloworld {
 		System.setProperty("webdriver.edge.driver", "C:\\Users\\chira\\eclipse-workspace\\Hello-World\\src\\test\\resources\\Driver\\msedgedriver.exe");
 		WebDriver edgedriver = new EdgeDriver();
 		edgedriver.get("https://www.flipkart.com/");
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\chira\\eclipse-workspace\\SeleniumTraining\\Hello-World\\src\\test\\resources\\Driver\\geckodriver.exe");
+		WebDriver fireFoxdriver = new FirefoxDriver();
+		edgedriver.get("https://www.amazon.com/");
+		
+		
 	}
 }
